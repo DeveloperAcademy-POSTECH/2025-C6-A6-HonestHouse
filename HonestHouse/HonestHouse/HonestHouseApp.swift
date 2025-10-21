bd2cf86eab2566ddf9300c52ecb3db72218b3789
Rama <gudrms2538@gmail.com>
2025-10-21 15:06:06 +0900

    chore: add project file

---
+++ HonestHouse/HonestHouse/HonestHouseApp.swift
@@ -0,0 +1,17 @@
+//
+//  HonestHouseApp.swift
+//  HonestHouse
+//
+//  Created by Rama on 10/11/25.
+//
+
+import SwiftUI
+
+@main
+struct HonestHouseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HosestHouse/HosestHouse/HosestHouseApp.swift
@@ -0,0 +1,17 @@
+//
+//  HosestHouseApp.swift
+//  HosestHouse
+//
+//  Created by Rama on 10/11/25.
+//
+
+import SwiftUI
+
+@main
+struct HosestHouseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
