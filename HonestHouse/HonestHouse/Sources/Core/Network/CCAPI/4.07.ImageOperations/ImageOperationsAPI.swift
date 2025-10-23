36efffdea178ccdb7a271883975e197eab216033
dlguszoo <lhjivy1024@gmail.com>
2025-10-23 17:52:07 +0900

    [#11] feat: ImageOperationsAPI 구현

---
+++ HonestHouse/HonestHouse/Sources/Core/Network/CCAPI/4.07.ImageOperations/ImageOperationsAPI.swift
@@ -0,0 +1,27 @@
+//
+//  ImageOperationsAPI.swift
+//  HonestHouse
+//
+//  Created by 이현주 on 10/23/25.
+//
+
+import Foundation
+
+enum ImageOperationsAPI {
+    case storage
+    case directory(String)
+    case contentsList(String, String)
+    
+    var apiDesc: String {
+        switch self {
+        case .storage:
+            "ver100/contents"
+            
+        case .directory(let storage):
+            "ver100/contents/\(storage)"
+            
+        case .contentsList(let storage, let directory):
+            "ver100/contents/\(storage)/\(directory)"
+        }
+    }
+}
