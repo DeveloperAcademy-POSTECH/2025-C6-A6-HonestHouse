8abf4037ecff6fe330198c0db1a0ccbdfa11ec9b
dlguszoo <lhjivy1024@gmail.com>
2025-10-23 18:35:41 +0900

    [#11] feat: ImageOperationsService 구현

---
+++ HonestHouse/HonestHouse/Sources/Service/CCAPI/ImageOperationsService.swift
@@ -0,0 +1,50 @@
+//
+//  ImageOperationsService.swift
+//  HonestHouse
+//
+//  Created by 이현주 on 10/23/25.
+//
+
+import Combine
+import CombineMoya
+import Moya
+
+protocol ImageOperationsServiceType {
+    /// storageList 조회
+    func getStorageList() async throws -> ImageOperations.StorageListResponse
+    
+    /// directoryList 조회
+    func getDirectoryList(storage: String) async throws -> ImageOperations.DirectoryListResponse
+    
+    /// contentList(이미지 리스트) 조회
+    func getContentList(storage: String, directory: String, type: String, kind: String, page: Int) async throws -> ImageOperations.ContentListResponse
+}
+
+final class ImageOperationsService: BaseService, ImageOperationsServiceType {
+    // MARK: - GET list of storage URLs
+    func getStorageList() async throws -> ImageOperations.StorageListResponse {
+        let response = try await requestWithRetry(ImageOperationsTarget.getStorageList, decoding: ImageOperations.StorageListResponse.self)
+        
+        print("Storage URLs: \(response.url ?? [])")
+        
+        return response
+    }
+
+    // MARK: - GET list of storage directorie URLs
+    func getDirectoryList(storage: String) async throws -> ImageOperations.DirectoryListResponse {
+        let response = try await requestWithRetry(ImageOperationsTarget.getDirectoryList(storage), decoding: ImageOperations.DirectoryListResponse.self)
+        
+        print("Directory URLs: \(response.url ?? [])")
+        
+        return response
+    }
+    
+    // MARK: - GET list of Content URLs
+    func getContentList(storage: String, directory: String, type: String, kind: String, page: Int) async throws -> ImageOperations.ContentListResponse {
+        let response = try await requestWithRetry(ImageOperationsTarget.getContentList(storage, directory, type, kind, page), decoding: ImageOperations.ContentListResponse.self)
+        
+        print("Content URLs: \(response.url ?? [])")
+        
+        return response
+    }
+}
