f69a9559755498f887f96db3f7fe59e8db10a808
Minbol <bmlee@postech.ac.kr>
2025-10-26 17:49:46 +0900

    API: #15 Picture Style 조회 및 설정
    * Feat: Picture Style endpoint 추가
    
    * Feat: PictureStyle 모델 추가
    
    * Feat: PictureStyleResponse DTO 추가
    
    - ShootingSettings 내 PictureStyleResponse 구조체 추가 (BaseResponse 준수)
    - PictureStyleResponse -> PictureStyle 변환 메서드(toEntity) 구현
    
    * Feat: getPictureStyle, putPictureStyle 추가

---
+++ HonestHouse/HonestHouse/Sources/Core/Model/4.9. Shooting Settings/28. PictureStyle.swift
@@ -0,0 +1,13 @@
+//
+//  PictureStyle.swift
+//  HonestHouse
+//
+//  Created by BoMin Lee on 10/23/25.
+//
+
+import Foundation
+
+struct PictureStyle {
+    let value: String?
+    let ability: [String]?
+}
