87dba3476d43ab20ffb00c10ed268e53cdf24085
Minbol <bmlee@postech.ac.kr>
2025-10-27 21:12:12 +0900

    Fix: MainViewModel 수정

--- HonestHouse/HonestHouse/Sources/Presentation/Main/ViewModel/MainViewModel.swift
+++ HonestHouse/HonestHouse/Sources/Presentation/Main/ViewModel/MainViewModel.swift
@@ -9,6 +9,41 @@ import SwiftUI
 
 @Observable
 final class MainViewModel {
-    var selectedSegment: MainViewSegmentType = .trishot
+    var selectedSegment: MainViewSegmentType = .trishot {
+        didSet {
+            if selectedSegment != oldValue {
+                exitEditMode()
+            }
+        }
+    }
+    
     var segments: [MainViewSegmentType] = [.trishot, .preset]
+    var isEditMode: Bool = false
+    var selectedDetailPreset: Preset?
+    var selectedEditorPreset: Preset?
+    var showingCreateSheet = false
+
+    var showEditButton: Bool {
+        selectedSegment == .preset
+    }
+
+    func toggleEditMode() {
+        isEditMode.toggle()
+    }
+
+    func exitEditMode() {
+        isEditMode = false
+    }
+
+    func showDetailView(for preset: Preset) {
+        selectedDetailPreset = preset
+    }
+
+    func showEditorView(for preset: Preset? = nil) {
+        selectedEditorPreset = preset
+    }
+
+    func showCreateSheet() {
+        showingCreateSheet = true
+    }
 }
