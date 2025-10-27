ba7f9d8cf60c5591f3d6cb7ddaafa47cf837d828
Minbol <bmlee@postech.ac.kr>
2025-10-27 21:14:44 +0900

    Feat: Services에 PresetService 추가

--- HonestHouse/HonestHouse/Sources/Service/Services.swift
+++ HonestHouse/HonestHouse/Sources/Service/Services.swift
@@ -6,10 +6,12 @@
 //
 
 import Foundation
+import SwiftData
 
 protocol ServiceType {
     var shootingSettingsService: ShootingSettingsServiceType { get set }
     var imageOperationsService: ImageOperationsServiceType { get set }
+    var presetService: PresetServiceType { get }
     var visionManager: VisionManagerType { get }
     var photoManager: PhotoManagerType { get }
 }
@@ -17,12 +19,14 @@ protocol ServiceType {
 class Services: ServiceType {
     var shootingSettingsService: ShootingSettingsServiceType
     var imageOperationsService: ImageOperationsServiceType
+    var presetService: PresetServiceType
     var visionManager: VisionManagerType
     var photoManager: PhotoManagerType
     
-    init() {
+    init(modelContext: ModelContext) {
         self.shootingSettingsService = ShootingSettingsService()
         self.imageOperationsService = ImageOperationsService()
+        self.presetService = PresetService(modelContext: modelContext)
         self.visionManager = VisionManager()
         self.photoManager = PhotoManager()
     }
