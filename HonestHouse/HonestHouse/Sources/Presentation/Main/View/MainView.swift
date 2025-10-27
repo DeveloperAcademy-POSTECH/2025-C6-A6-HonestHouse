8c9a92d62a0e50f1b8904624f3d42ac332f1215b
Minbol <bmlee@postech.ac.kr>
2025-10-27 21:12:34 +0900

    Fix: MainView 수정

--- HonestHouse/HonestHouse/Sources/Presentation/Main/View/MainView.swift
+++ HonestHouse/HonestHouse/Sources/Presentation/Main/View/MainView.swift
@@ -6,19 +6,85 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct MainView: View {
-    @State var vm: MainViewModel = .init()
+    @EnvironmentObject private var container: DIContainer
+    @Environment(\.modelContext) private var modelContext
+    @State private var vm: MainViewModel = .init()
     
     var body: some View {
-        VStack {
-            cameraAndArchiveHeaderView()
-            Spacer().frame(height: 18)
-            segmentedControlView()
-            Spacer().frame(height: 27)
-            // TODO: 트라이샷, 프리셋 뷰 연결 필요
+        NavigationStack {
+            VStack(spacing: 18) {
+                cameraAndArchiveHeaderView()
+                segmentedControlView()
+                
+                Group {
+                    switch vm.selectedSegment {
+                    case .trishot:
+                        Text("TriShot View")
+                    case .preset:
+                        PresetView(
+                            container: container,
+                            isEditMode: $vm.isEditMode,
+                            onShowDetail: vm.showDetailView,
+                            onShowEditor: vm.showEditorView,
+                            onShowCreate: vm.showCreateSheet
+                        )
+                    }
+                }
+                .padding(.top, 9)
+            }
+            .padding(.horizontal, 16)
+            .navigationDestination(item: $vm.selectedDetailPreset) { preset in
+                PresetDetailView(
+                    preset: preset,
+                    onShowEditor: vm.showEditorView
+                )
+            }
+            .navigationDestination(item: $vm.selectedEditorPreset) { preset in
+                PresetEditorTemporaryView(
+                    preset: preset,
+                    onSave: { name, mode, pictureStyle, aperture, shutterSpeed, iso, exposureCompensation, colorTemperature, tintBlueAmber, tintMagentaGreen in
+                        preset.name = name
+                        preset.shootingMode = mode
+                        preset.pictureStyle = pictureStyle
+                        preset.aperture = aperture
+                        preset.shutterSpeed = shutterSpeed
+                        preset.iso = iso
+                        preset.exposureCompensation = exposureCompensation
+                        preset.colorTemperature = colorTemperature
+                        preset.tintBlueAmber = tintBlueAmber
+                        preset.tintMagentaGreen = tintMagentaGreen
+                        preset.updatedAt = Date()
+                        try? modelContext.save()
+                    },
+                    onDelete: {
+                        modelContext.delete(preset)
+                        try? modelContext.save()
+                        vm.selectedEditorPreset = nil
+                    }
+                )
+            }
+            .sheet(isPresented: $vm.showingCreateSheet) {
+                PresetEditorTemporaryView { name, mode, pictureStyle, aperture, shutterSpeed, iso, exposureCompensation, colorTemperature, tintBlueAmber, tintMagentaGreen in
+                    let newPreset = Preset(
+                        name: name,
+                        pictureStyle: pictureStyle,
+                        shootingMode: mode,
+                        aperture: aperture,
+                        shutterSpeed: shutterSpeed,
+                        iso: iso,
+                        exposureCompensation: exposureCompensation,
+                        colorTemperature: colorTemperature,
+                        tintBlueAmber: tintBlueAmber,
+                        tintMagentaGreen: tintMagentaGreen
+                    )
+                    modelContext.insert(newPreset)
+                    try? modelContext.save()
+                }
+            }
         }
-        .padding(.horizontal, 16)
     }
     
     private func cameraAndArchiveHeaderView() -> some View {
@@ -26,13 +92,30 @@ struct MainView: View {
             Button {
                 // TODO: 카메라 설정 뷰 연결
             } label: {
-                Image(.cameraAdd)
+                Image(systemName: "camera.badge.ellipsis")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 24)
             }
             Spacer()
+
+            if vm.showEditButton {
+                Button {
+                    vm.toggleEditMode()
+                } label: {
+                    Text(vm.isEditMode ? "완료" : "편집")
+                        .font(.system(size: 16, weight: .bold))
+                }
+                .padding(.trailing, 12)
+            }
+
             Button {
-                // TODO: 아카이빙 뷰 연결
+                // TODO: 사진 불러오기 연결
             } label: {
-                Image(.squareGrid)
+                Image(systemName: "photo.badge.plus")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 24)
             }
         }
         .foregroundStyle(.black)
