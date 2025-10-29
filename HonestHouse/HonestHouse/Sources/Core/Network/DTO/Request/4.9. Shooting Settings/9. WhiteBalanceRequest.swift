8a1ba9d1dcc40af3b004b5aa24fb8bcb83e3bc8f
Minbol <bmlee@postech.ac.kr>
2025-10-29 20:33:32 +0900

    feat: WhiteBalanceRequest 생성

---
+++ HonestHouse/HonestHouse/Sources/Core/Network/DTO/Request/4.9. Shooting Settings/9. WhiteBalanceRequest.swift
@@ -0,0 +1,12 @@
+//
+//  WhiteBalanceRequest.swift
+//  HonestHouse
+//
+//  Created by BoMin Lee on 10/28/25.
+//
+
+extension ShootingSettings {
+    struct WhiteBalanceRequest: BaseRequest {
+        let value: String
+    }
+}
