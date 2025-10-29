e21e6df07fd7c4481ba4c017ca57e2ab900b22b9
Minbol <bmlee@postech.ac.kr>
2025-10-29 12:53:03 +0900

    feat: ShootingControlService 추가

--- HonestHouse/HonestHouse/Sources/Service/Services.swift
+++ HonestHouse/HonestHouse/Sources/Service/Services.swift
@@ -9,14 +9,16 @@ import Foundation
 import SwiftData
 
 protocol ServiceType {
+    var shootingControlService: ShootingControlServiceType { get set }
     var shootingSettingsService: ShootingSettingsServiceType { get set }
     var imageOperationsService: ImageOperationsServiceType { get set }
-    var presetService: PresetServiceType { get }
+    var presetService: PresetServiceType { get set }
     var visionManager: VisionManagerType { get }
     var photoManager: PhotoManagerType { get }
 }
 
 class Services: ServiceType {
+    var shootingControlService: ShootingControlServiceType
     var shootingSettingsService: ShootingSettingsServiceType
     var imageOperationsService: ImageOperationsServiceType
     var presetService: PresetServiceType
@@ -24,6 +26,7 @@ class Services: ServiceType {
     var photoManager: PhotoManagerType
     
     init(modelContext: ModelContext) {
+        self.shootingControlService = ShootingControlService()
         self.shootingSettingsService = ShootingSettingsService()
         self.imageOperationsService = ImageOperationsService()
         self.presetService = PresetService(modelContext: modelContext)
