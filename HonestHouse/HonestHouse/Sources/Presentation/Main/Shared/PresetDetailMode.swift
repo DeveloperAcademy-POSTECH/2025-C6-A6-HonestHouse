3e261086d569353388be70f79e0da14b07923e12
Minbol <bmlee@postech.ac.kr>
2025-10-29 20:23:02 +0900

    feat: PresetDetailMode 생성

---
+++ HonestHouse/HonestHouse/Sources/Presentation/Main/Shared/PresetDetailMode.swift
@@ -0,0 +1,14 @@
+//
+//  PresetDetailMode.swift
+//  HonestHouse
+//
+//  Created by BoMin Lee on 10/29/25.
+//
+
+import Foundation
+
+enum PresetDetailMode {
+    case view      // 조회 모드
+    case edit      // 편집 모드
+    case create    // 생성 모드
+}
