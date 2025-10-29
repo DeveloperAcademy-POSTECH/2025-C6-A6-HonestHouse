d028f2a2935c294d7fe4ac9d84004b4ea4d40f96
Minbol <bmlee@postech.ac.kr>
2025-10-29 12:52:31 +0900

    feat: IgnoreShootingModeRequest

---
+++ HonestHouse/HonestHouse/Sources/Core/Network/DTO/Request/4.8. Shooting Control/3. IgnoreShootingModeRequest.swift
@@ -0,0 +1,14 @@
+//
+//  IgnoreShootingModeRequest.swift
+//  HonestHouse
+//
+//  Created by BoMin Lee on 10/29/25.
+//
+
+import Foundation
+
+extension ShootingControl {
+    struct IgnoreShootingModeRequest: BaseRequest {
+        let action: String
+    }
+}
