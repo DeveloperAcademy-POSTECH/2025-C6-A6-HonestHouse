9e0256e085315382ddbccf854cfe2d00f438469f
realhsb <realhsb@gmail.com>
2025-10-29 20:55:39 +0900

    feat: Preset Detail View Mode 관리

---
+++ HonestHouse/HonestHouse/Sources/Core/Managers/Shared/PresetDetailMode.swift
@@ -0,0 +1,12 @@
+//
+//  PresetDetailMode.swift
+//  HonestHouse
+//
+//  Created by Subeen on 10/29/25.
+//
+
+enum PresetDetailMode {
+    case view      // 조회 모드
+    case edit      // 편집 모드
+    case create    // 생성 모드
+}
