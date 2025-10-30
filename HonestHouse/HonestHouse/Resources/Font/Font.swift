07dcac21f46afd5a9a173add40067a9a3f742828
realhsb <realhsb@gmail.com>
2025-10-30 19:34:26 +0900

    design: Font extension 정의

--- HonestHouse/HonestHouse/Resources/Font/Font.swift
+++ HonestHouse/HonestHouse/Resources/Font/Font.swift
@@ -5,4 +5,26 @@
 //  Created by Subeen on 10/22/25.
 //
 
-import Foundation
+import SwiftUI
+
+extension Font {
+    static let headlineS: Font = .custom("Pretendard-Bold", size: 28)   /// Pretendard Bold 28 140%
+ 
+    static let title1: Font = .custom("Pretendard-Bold", size: 24)      /// Pretendard Bold 24 140%
+    static let title2: Font = .custom("Pretendard-Bold", size: 20)      /// Pretendard Bold 20 140%
+    static let title3: Font = .custom("Pretendard-Bold", size: 18)      /// Pretendard Bold 18 140%
+ 
+    static let lableL: Font = .custom("Pretendard-Bold", size: 16)      /// Pretendard Bold 16 140%
+    static let lableM: Font = .custom("Pretendard-Bold", size: 14)      /// Pretendard Bold 14 140%
+ 
+    static let body1: Font = .custom("Pretendard-SemiBold", size: 16)   /// Pretendard SemiBold 16 140%
+    static let body2: Font = .custom("Pretendard-SemiBold", size: 14)   /// Pretendard SemiBold 14 140%
+ 
+    static let captionL: Font = .custom("Pretendard-Regular", size: 16) /// Pretendard Regular 16
+    static let captionM: Font = .custom("Pretendard-Regular", size: 14) /// Pretendard Regular 14 140%
+ 
+    static let num1: Font = .custom("SFMonoSemiBold", size: 20)         /// SF Mono SemiBold 20  130%
+    static let num2: Font = .custom("SFMonoSemiBold", size: 18)         /// SF Mono SemiBold 18  130%
+    static let num3: Font = .custom("SFMonoMedium", size: 16)           /// SF Mono Medium 16  130%
+    static let num4: Font = .custom("SFMonoMedium", size: 14)           /// SF Mono Medium 14  130%
+}
